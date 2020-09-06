bb47e7e0b3a8541bf2481239a8bb541f0bae32df
hftamayo <hftamayo@gmail.com>
2020-09-06 16:26:48 -0600

    ejercicio de agrupacion de operaciones

---
+++ refuerzo/E05Agrupacion.java
@@ -0,0 +1,13 @@
+package refuerzo;
+
+public class E05Agrupacion {
+    public static void main(String args[]){
+        int a = 1 + 3 * 4; 
+        int b = (1 + 3) * 4; 
+        
+        int c = ((12 - 2) * ( 21 - 11)) / ((1+1)*(15-10)) + 1 ;
+        System.out.println("primera operacion: "+a);
+        System.out.println("primera operacion: "+b);
+        System.out.println("primera operacion: "+c);
+    }    
+}
